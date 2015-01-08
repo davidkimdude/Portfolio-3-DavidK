25ef438b594bbe762af32288de8733a5021c1a4e
David Kim <hhjj0506@gmail.com>
2015-01-08 15:01:30 -0800

    1/8/15

--- public_html/script.js
+++ public_html/script.js
@@ -1,6 +1,12 @@
 $("document").ready(function(){
-    $(".siblings").css("background-color", "orange");
+    $('.siblings').css('background-color', 'orange');
     
-    $(".background").css("background-color", "green");
+    $('.background').css('background-color', 'black');
+    
+    $('p:nth-child(1)').css('background-color', 'pink');
+    
+    $('p:last').css('color', 'red');
+    
+    $('p:gt(0)').css('border-style', 'solid');
     
 });
