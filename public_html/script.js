56f154a5d231457d8062da5e1819511d1dcf05d5
David Kim <hhjj0506@gmail.com>
2015-01-09 15:02:39 -0800

    1/9/15

--- public_html/script.js
+++ public_html/script.js
@@ -1,4 +1,8 @@
 $("document").ready(function(){
+    $('h1').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
+    
+    $('h1').bind('click', mouseClick);
+    
     $('.siblings').css('background-color', 'orange');
     
     $('.background').css('background-color', 'black');
@@ -10,3 +14,15 @@ $("document").ready(function(){
     $('p:gt(0)').css('border-style', 'solid');
     
 });
+
+function mouseOverMe(){
+    $('h1').html("Bruh");
+}
+
+function mouseOutMe(){
+    $('h1').html("You");
+}
+
+function mouseClick(){
+    $('p').html('Wazzup');
+}
