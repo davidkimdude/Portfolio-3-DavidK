e0791c18b784c5778b72041827a35392830db5f1
David Kim <hhjj0506@gmail.com>
2015-01-16 14:56:22 -0800

    1/16/15

--- public_html/home.js
+++ public_html/home.js
@@ -1,5 +1,6 @@
 $('document').ready(function(){
-   $('.body').css('background-image', 'url()'); 
+   $('.background').css('background-image', 'url(http://th09.deviantart.net/fs32/PRE/f/2008/220/c/9/spray_city_background_by_PhenomenallyMe22.jpg)');
+   $('#superHumans').accordion({header: 'h3'});
 });
 
 
