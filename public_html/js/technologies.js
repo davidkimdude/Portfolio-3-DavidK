574ebc2baab70dfecda46fc3a058fccd67564e19
David Kim <hhjj0506@gmail.com>
2015-01-22 14:58:47 -0800

    Final

--- public_html/js/technologies.js
+++ public_html/js/technologies.js
@@ -1,3 +1,30 @@
 $('document').ready(function(){
    
 });
+
+//Make a button that pauses a background video
+var vid = document.getElementById("bgvid");
+var pauseButton = document.getElementById("vidpause");
+function vidFade() {
+vid.classList.add("stopfade");
+}
+vid.addEventListener('ended', function() {
+// only functional if "loop" is removed
+vid.pause();
+// to capture IE10
+vidFade();
+});
+//Add an event listner that pauses the video by clicking
+pauseButton.addEventListener("click", function() {
+vid.classList.toggle("stopfade");
+//If you click the button again when video is paused, play the video
+if (vid.paused) {
+vid.play();
+//When video is playing button says 'Pause'
+pauseButton.innerHTML = "Pause";
+} else {
+vid.pause();
+//When video is paused buttons says 'Paused'
+pauseButton.innerHTML = "Paused";
+}
+});
--- public_html/script.js
+++ public_html/script.js
@@ -1,22 +1,22 @@
 $("document").ready(function(){
     $('#background').css('background-color', 'gold');
-    
+    //If mouuse is over, show the different text
     $('h1').bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-    
+    //Make the h1 clickable
     $('h1').bind('click', mouseClick);
-    
+    //Set the class siblings' background orange
     $('.siblings').css('background-color', 'orange');
-    
+    //Make the background color of nth child of the p pink
     $('p:nth-child(1)').css('background-color', 'pink');
-    
+    //Make the last p code's color red
     $('p:last').css('color', 'red');
-    
+    //gt of p's border appears
     $('p:gt(0)').css('border-style', 'solid');
-    
+    //If you click something, it will replace the text
     $('#replaceWText').bind('click', replaceWText);
-    
+    //Click and add another text
     $('#randPara').bind('click', addAPara);
-    
+    //Click certain thing and remove text
     $('#removePara').bind('click', removeAPara);
     
     $('#hideLogo').bind('click', hideTheImage);
@@ -32,38 +32,47 @@ $("document").ready(function(){
 });
 
 function removeAPara(){
+    //If you click last p code the texts are removed
     $('#randPara p:last').remove();
 }
 
 function addAPara(){
+    //If p is added show 'Added'
     $('#randPara').append('<p>Added</p>');
 }
 
 function replaceWText(){
+    //If text replaced show 'Changed'
     $('#replaceWText').text('Changed');
 }
 
 function mouseOverMe(){
+    //If mouse is over h1 tag show'Bruh'
     $('h1').html("Bruh");
 }
 
 function mouseOutMe(){
+    //If mouse is out of h1 tag show 'You'
     $('h1').html("You");
 }
 
 function mouseClick(){
+    //If you click p tag show 'Wazzup'
     $('p').html('Wazzup');
 }
 
 function hideTheImage(){
+    //If you click the button hide the logo in explode style, in 2500milliseconds
     $('#logo').hide('explode', {}, 2500);
 }
 
 function showTheImage(){
+    //If you click the button show the logo in explode style, in 2500milliseconds
     $('#logo').show('explode', {}, 2500);
 }
 
 function toggleTheText(){
+    //If you click the button toggle the text in 2500milliseconds
     $('h4').toggle(2500);
 }
 
